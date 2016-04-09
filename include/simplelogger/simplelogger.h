d4ab98e5383a3d64c6fd726aa5301b5759ee8857
Natesh Narain <nnaraindev@gmail.com>
2016-04-08 23:33:01 -0400

    specifc custom path

--- include/simplelogger/simplelogger.h
+++ include/simplelogger/simplelogger.h
@@ -10,6 +10,10 @@
 #ifndef SIMPLE_LOGGER_H
 #define SIMPLE_LOGGER_H
 
+#ifndef FILE_NAME
+#	define FILE_NAME __FILE__
+#endif
+
 #include "logger/logger_base.h"
 #include "logger/level.h"
 
@@ -19,13 +23,13 @@
 */
 
 //! Log informational message
-#define LOG_INFO(msg, ...)  simplelogger::log(Level::INFO,  __FILE__ ":%d" " [INFO ] " msg "\n", __LINE__, ##__VA_ARGS__)
+#define LOG_INFO(msg, ...)  simplelogger::log(simplelogger::Level::INFO,  FILE_NAME ":%d" " [INFO ] " msg "\n", __LINE__, ##__VA_ARGS__)
 //! Log debug message
-#define LOG_DEBUG(msg, ...) simplelogger::log(Level::DEBUG, __FILE__ ":%d" " [DEBUG] " msg "\n", __LINE__, ##__VA_ARGS__)
+#define LOG_DEBUG(msg, ...) simplelogger::log(simplelogger::Level::DEBUG, FILE_NAME ":%d" " [DEBUG] " msg "\n", __LINE__, ##__VA_ARGS__)
 //! Log warning message
-#define LOG_WARN(msg, ...)  simplelogger::log(Level::WARN,  __FILE__ ":%d" " [WARN ] " msg "\n", __LINE__, ##__VA_ARGS__)
+#define LOG_WARN(msg, ...)  simplelogger::log(simplelogger::Level::WARN,  FILE_NAME ":%d" " [WARN ] " msg "\n", __LINE__, ##__VA_ARGS__)
 //! Log error message
-#define LOG_ERROR(msg, ...) simplelogger::log(Level::ERROR, __FILE__ ":%d" " [ERROR] " msg "\n", __LINE__, ##__VA_ARGS__)
+#define LOG_ERROR(msg, ...) simplelogger::log(simplelogger::Level::ERROR, FILE_NAME ":%d" " [ERROR] " msg "\n", __LINE__, ##__VA_ARGS__)
 
 /** @} */
 
